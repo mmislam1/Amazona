8db2497341d216696e4c5baac8dfebe771f21254
mmislam1 <36233551+mmislam1@users.noreply.github.com>
2022-01-11 00:49:10 +0600

    24 end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import SigninScreen from './screens/SigninScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ShippingAdressScreen from './screens/ShippingAdressScreen'
 function App() {
   const cart = useSelector((state) => state.cart)
   const cartItems = cart.cartItems
@@ -58,6 +59,7 @@ function App() {
           <Route path='/signin' component={SigninScreen}></Route>
           <Route path='/register' component={RegisterScreen}></Route>
           <Route path='/' component={HomeScreen} exact></Route>
+          <Route path='/shipping' component={ShippingAdressScreen}></Route>
         </main>
 
         <footer className='row center'>All rights reserved</footer>
